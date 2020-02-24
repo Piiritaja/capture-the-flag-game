9e5d35b0e685ca907b5987aa7f1c1d2714df8d25
sesuur <sesuur@ttu.ee>
2020-02-24 14:45:17 +0200

    Added another testmap, changed map loading logic

--- src/game/maps/MapLoad.java
+++ src/game/maps/MapLoad.java
@@ -10,18 +10,16 @@ import javafx.stage.WindowEvent;
 
 public class MapLoad extends Application {
 
-    private static double height = 720;
-    private static double width = 1280;
-    static Scene scene;
+    final double ASPECT_RATIO = 1.6;
 
     @Override
     public void start(Stage stage) {
 
-        StackPane root = new StackPane();
+        /*StackPane root = new StackPane();
         root.setId("pane");
 
         scene = new Scene(root, width, height);
-        scene.getStylesheets().add("assets/map/2teams/map1.css");
+        scene.getStylesheets().add("assets/map/2teams/map1/map1.css");
 
         stage.setTitle("MapLoad");
         //Adding scene to the stage
@@ -29,6 +27,12 @@ public class MapLoad extends Application {
         stage.setResizable(false);
 
         //Displaying the contents of the stage
+        stage.show();*/
+        // Scene map = loadMap1();
+        Scene map = loadMap2();
+        stage.setScene(map);
+        stage.minWidthProperty().bind(map.heightProperty().multiply(ASPECT_RATIO));
+        stage.minHeightProperty().bind(map.widthProperty().divide(ASPECT_RATIO));
         stage.show();
 
         stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
@@ -40,8 +44,33 @@ public class MapLoad extends Application {
         });
 
     }
+
     public static void main(String args[]) {
         launch(args);
 
+    }
+
+    public Scene loadMap2() {
+        StackPane root = new StackPane();
+
+        MapLayer floor = new MapLayer("assets/map/2teams/map2/floor.png");
+        // floor.png is 1280x800
+        floor.addToPane(root);
+
+        MapLayer colors = new MapLayer("assets/map/2teams/map2/colors.png");
+        colors.addToPane(root);
+
+        MapLayer walls = new MapLayer("assets/map/2teams/map2/walls.png");
+        walls.addToPane(root);
+        return new Scene(root);
+    }
+
+    public Scene loadMap1() {
+        StackPane root = new StackPane();
+        root.setId("pane");
+
+        Scene scene = new Scene(root, 1280, 800);
+        scene.getStylesheets().add("assets/map/2teams/map1/map1.css");
+        return scene;
     }
 }
